2cdf7cc7b21e4ca5a291ab53670040553809a956
JESUS DAVID REINA MARTINEZ <jdavidreina@ucundinamarca.edu.co>
2026-06-29 12:16:31 -0500

    Agregar archivos de proyecto.

---
+++ AngelesPizza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AngelesPizza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
